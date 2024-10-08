2622043814f5980ac9f9b908e6448afe045db7bc
michele25-10 <michele.gabri01@gmail.com>
2024-10-08 14:07:24 +0200

    Customize di react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../src/styles/global.scss';
-import Text from './pages/test/Text';
+import { Col, Container, Row, Button, Stack } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-
-      Template react app
-      <Text />
-    </div>
+    <Stack direction="horizontal" gap={2}>
+      <Button variant="primary">
+        Button as link
+      </Button>
+      <Button variant="secondary">
+        Button as link
+      </Button>
+    </Stack>
   );
 }
 
